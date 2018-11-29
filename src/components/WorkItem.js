05cf7f268d37e2f416eb16a4c34bafae5c30b2f9
Bradley Jinks <Bradley.Jinks123@outlook.com>
2018-11-29 10:04:51 +0000

    update work items

--- src/components/WorkItem.js
+++ src/components/WorkItem.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 export default class WorkItem extends Component {
 
   render() {
-    const {name,description,homepage}=this.props.repo;
+    const {name,description,homepage,html_url}=this.props.repo;
     let {language} = this.props.repo;
     language = language==='CSS' ? 'Javascript' : language
-    const header = homepage ? <h3><a className="icon fa-github" href={homepage}>{name} | {language}</a></h3> : <h3>{name} | {language}</h3>
+    const header = homepage ? <h3><a className="icon fa-globe" href={homepage}>{name} | {language}</a></h3> : <h3>{name} | {language} | <a className="icon fa-github" href={html_url}>Code</a></h3>
     
     return (
       <React.Fragment>
